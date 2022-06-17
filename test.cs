a1fe2e259a23c5122782db4a299b564b5521b031
Priyanka Miranda <priyankamiranda2@gmail.com>
2022-06-16 20:32:57 -0400

    test

--- test.cs
+++ test.cs
@@ -7,7 +7,6 @@
 ///     var = GetValueOf(9f);
 /// @endcode
 /// </example>
-/// <param name="_myParam">A float value to return</param>
 public float GetValueOf(float _myParam)
 {
 	return _myParam;
